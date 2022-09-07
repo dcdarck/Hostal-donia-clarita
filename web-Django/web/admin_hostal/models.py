81b4caf4259c7ec4f52c848b2bfb8faca2481fbe
agustinllana <ag.llana@duocuc.cl>
2022-09-06 22:09:40 -0300

    sssss

--- web-Django/web/admin_hostal/models.py
+++ web-Django/web/admin_hostal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Proveedor(models.Model):
+    ID  = models.AutoField(primary_key=True),
+    Nombre= models.CharField(max_length=100, verbose_name='Nombre'),
+    Email = models.CharField(max_length=100, verbose_name='Email'),
+    Telefono = models.CharField(max_length=100, verbose_name='Telefono'),
+    Direccion = models.CharField(max_length=100, verbose_name='Direccion'),
+    Rubro = models.CharField(max_length=100, verbose_name='Rubro')
